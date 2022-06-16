edac28cf7de5a050ff953a9899e5a281e8043191
alexbean84 <alexbean84@gmail.com>
2022-06-17 00:33:27 +0300

    Create a global variable which is needed by the computer in the function named computerChoice in order to pick either rock, paper or scissors and three other functions that are going to be used later on

---
+++ app.js
@@ -0,0 +1,19 @@
+const choices = ['rock', 'paper', 'scissors'];
+
+function game() {
+
+}
+
+function playRound() {
+    const playerSelection =  
+}
+
+function playerChoice() {
+
+}
+
+function computerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+
