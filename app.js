611998efa0209b471066386e44c5c21113c437f0
alexbean84 <alexbean84@gmail.com>
2022-06-17 00:59:27 +0300

    Make the user input case insensitive

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const choices = ['rock', 'paper', 'scissors'];
 
 function game() {
-
+    playRound();
 }
 
 function playRound() {
-    const playerSelection =  
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice(); 
 }
 
 function playerChoice() {
+    let input = prompt('Rock, Paper or Scissors?');
+    input = input.toLowerCase();
+    console.log(input);
 
 }
 
 function computerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)]
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
+game();
 
